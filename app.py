12c071be04b74cb0b54f1f1ed4922fe7f7e12789
ABHISHEK SUNIL SONAWANE <114007055+sonawane2162@users.noreply.github.com>
2026-02-19 09:28:02 +0530

    Refactor API_KEY handling and improve error handling

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-56230be41a7944abac39a36dc0ce4fd3
+
 from flask import Flask
 import requests
+import os
 
 app = Flask(__name__)
 
-API_KEY = "56230be41a7944abac39a36dc0ce4fd3"
+API_KEY = os.environ.get("56230be41a7944abac39a36dc0ce4fd3")
 
 @app.route("/")
 def home():
@@ -14,12 +15,13 @@ def home():
 
     news_html = "<h1>Live India News</h1>"
 
-    for article in data["articles"][:10]:
-        news_html += f"<h3>{article['title']}</h3>"
-        news_html += f"<p>{article['description']}</p>"
-        news_html += "<hr>"
+    if "articles" in data:
+        for article in data["articles"][:10]:
+            title = article.get("title", "")
+            desc = article.get("description", "")
+            news_html += f"<h3>{title}</h3>"
+            news_html += f"<p>{desc}</p><hr>"
+    else:
+        news_html += "<p>Error fetching news.</p>"
 
     return news_html
-
-if __name__ == "__main__":
-    app.run()
